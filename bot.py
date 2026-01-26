76deab4c52608ae702ed8efd07bf4d7bca9ecb6b
vipne2025-coder <vipne2025@gmail.com>
2026-01-26 21:39:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,10 @@ def send_telegram(text: str) -> None:
     r = requests.post(url, data={"chat_id": CHAT_ID, "text": text}, timeout=20)
     r.raise_for_status()
 
-if name == "main":
+if __name__ == "main":
     send_telegram("Bot started")
 
     while True:
         print("Tick", flush=True)
         time.sleep(15)
+
